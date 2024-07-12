22de7974fa88fd77f4f26499900facd95e2fc100
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-07-11 22:19:48 -0600

    chore: Update next.config to use dynamic base path from repository configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
+import { REPO } from "./config/repo";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // "export" allows for static site generation
     output: "export",
     // "basePath" is used to set the base path for the project.
     // Since the project is hosted on GitHub Pages, the base path is the repository name.
-    basePath: "pigmentocracia",
+    basePath: REPO,
     // Disable image optimization, since we don't have a Node.js server to handle it.
     images: {
         unoptimized: true
