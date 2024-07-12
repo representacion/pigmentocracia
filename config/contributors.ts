cd8ac0f785c7d476c593de391c3cbf17824a956d
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-07-11 22:09:00 -0600

    feat: Add contributors configuration file

---
+++ config/contributors.ts
@@ -0,0 +1,18 @@
+import { Contributor } from "@/types/config/contributors";
+
+// This constant holds the list of contributors to the project
+// that are not registered as contributors in the repository
+const projectContributors: Contributor[] = [
+    {
+        screenName: "Ana Padilla",
+        href: "",
+        name: "Ana Padilla"
+    },
+    {
+        screenName: "Oliver Agiss",
+        href: "",
+        name: "Oliver Agiss"
+    }
+];
+
+export { projectContributors };
