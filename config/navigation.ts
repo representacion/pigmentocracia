6d4521af584be6460972d62a808700337b1391dd
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-07-15 19:34:19 -0600

    Feat: Add pages (navigation) configuration object (i.e., an object that describes the pages to be rendered in any navigation component)

---
+++ config/navigation.ts
@@ -0,0 +1,14 @@
+import { Page } from "@/types/config/pages";
+
+/**
+ * @constant {Page[]} PAGES - The pages to display in the navigation elements.
+ */
+const PAGES: Page[] = [
+    {
+        title: "Inicio",
+        id: "home",
+        href: "/"
+    }
+];
+
+export { PAGES };
