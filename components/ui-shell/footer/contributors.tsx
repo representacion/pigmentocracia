b7c4b61567b7d44dbd581e10c95c039e16630214
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-07-16 08:37:33 -0600

    feat: Add support for Gobernantes Info and Morlan logos in the Contributors component

--- components/ui-shell/footer/contributors.tsx
+++ components/ui-shell/footer/contributors.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useMemo } from "react";
+import Image from "next/image";
 import { useContributors } from "@/lib/contributors/useContributors";
+import GobernantesInfoLogo from "@/public/images/gobernantesinfo-logo.svg";
+import MorlanLogo from "@/public/images/morlan-logo.svg";
+import Link from "next/link";
 
 const Contributors = () => {
 
@@ -36,6 +40,17 @@ const Contributors = () => {
             <p className="text-center">
                 Hecho con ❤️ por <span className="inline-flex gap-1">{contributorsLinks}</span>.
             </p>
+            <p>
+                Con el apoyo de
+            </p>
+            <div className="flex flex-col items-center align-baseline gap-4">
+                <Link href="https://www.gobernantes.info" target="_blank">
+                    <Image src={GobernantesInfoLogo} alt="Gobernantes Info Logo" height={48} />
+                </Link>
+                <Link href="https://www.morlan.mx/" target="_blank">
+                    <Image src={MorlanLogo} alt="Morlan Logo" height={48} />
+                </Link>
+            </div>
         </>
     );
 };
