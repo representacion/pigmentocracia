eaddd692c8bf06b26000ee8d730c0742690d9d3b
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2024-07-15 20:19:09 -0600

    Fix: Add missing href property to link

--- components/ui-shell/navbar/brand.tsx
+++ components/ui-shell/navbar/brand.tsx
@@ -1,7 +1,7 @@
 const Brand = ({ }) => {
 
     return (
-        <a className="btn btn-ghost text-xl uppercase gap-x-0">
+        <a href="/" className="btn btn-ghost text-xl uppercase gap-x-0">
             <span className="text-black">Pigmento</span><span className="text-yellow-800">cracia</span>
         </a>
     )
