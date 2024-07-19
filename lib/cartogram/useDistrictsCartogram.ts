2971ee201cf3a26ad95318e917e99ac51fb2a103
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2024-07-19 17:19:54 -0600

    Chore: Add cartogram GeoJSON properties type

--- lib/cartogram/useDistrictsCartogram.ts
+++ lib/cartogram/useDistrictsCartogram.ts
@@ -1,22 +1,21 @@
-import { useEffect, useMemo, useState } from "react";
-import L, { Map } from "leaflet";
+import useSWR from "swr";
 import { useCartogram } from "@/lib/cartogram/useCartogram";
 import { REPO } from "@/config/repo";
-import { unMountMap, unMountLayer } from "@/lib/leaflet/map/utils";
+import type { DistrictsCartogramGeoJsonProperties } from "@/types/cartogram";
 
 const useDistrictsCartogram = ({ }) => {
 
     const { 
         cartogram: districtsGeoJson,
         isLoading: districtsGeoJsonIsLoading
-    } = useCartogram({
+    } = useCartogram<DistrictsCartogramGeoJsonProperties>({
         path: `${REPO}/data/mexico300-2024.geojson`
     });
 
     const { 
         cartogram: statesGeoJson,
         isLoading: statesGeoJsonIsLoading
-    } = useCartogram({
+    } = useCartogram<DistrictsCartogramGeoJsonProperties>({
         path: `${REPO}/data/mexico300bordes-2024.geojson`
     });
 
---
+++ types/cartogram/index.ts
@@ -0,0 +1,4 @@
+export type DistrictsCartogramGeoJsonProperties = {
+    estado: string;
+    distrito: string;
+}
