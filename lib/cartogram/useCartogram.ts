be33b10514fd8d08b4f0d07fde81e677cbd4f855
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2024-07-19 17:26:48 -0600

    Refactor useCartogram to remove unused code and improve readability

--- lib/cartogram/useCartogram.ts
+++ lib/cartogram/useCartogram.ts
@@ -1,18 +1,15 @@
-import { useRef } from "react";
 import axios from "axios";
 import useSWR, { Fetcher } from "swr";
 import type { FeatureCollection, Polygon } from "geojson";
 
-interface useCartogramArgs<Properties> {
+interface useCartogramArgs {
     /**
      * The path to the cartogram file (assumed to be a GeoJSON file) with a `FeatureCollection` of `Polygon` features).
      */
     path: string;
 };
 
-const useCartogram = <Properties extends Record<any, any>>({ path }: useCartogramArgs<Properties>) => {
-
-    const svgRef = useRef<SVGSVGElement>(null);
+const useCartogram = <Properties extends Record<any, any>>({ path }: useCartogramArgs) => {
 
     // Here, I type the fetcher function to ensure that the data returned is a `FeatureCollection` of `Polygon` features.
     const fetcher: Fetcher<FeatureCollection<Polygon, Properties>, string> = async (path: string) => {
@@ -23,7 +20,6 @@ const useCartogram = <Properties extends Record<any, any>>({ path }: useCartogra
     const { data, isLoading } = useSWR(path, fetcher);
 
     return {
-        svgRef,
         cartogram: data,
         isLoading
     }
