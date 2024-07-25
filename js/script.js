135d1fcc7bc194dab2d16aaa191c98b78c64cef5
Martín Szyszlican <martin@abrimos.info>
2024-07-24 21:23:49 -0600

    Mejorar clasificación. Agregar listado abajo de fotos y colores.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,5 @@
-
-
-// const apiUrl = 'https://prep2024.ine.mx/publicacion/nacional/assets/diputaciones/mapas/nacional/nacional.json';
-// const url = 'https://corsproxy.io/?' + encodeURIComponent(apiUrl) + "?rand="+Math.random();
-// const avanceApiUrl = 'https://prep2024.ine.mx/publicacion/nacional/assets/diputaciones/avanceNacional.json';
-// const avanceUrl = 'https://corsproxy.io/?' + encodeURIComponent(avanceApiUrl)  + "?rand="+Math.random();
-
-
 let ganadores = new Map();
 let avance = new Map();
-
-// let datos = fetch(url).then(response => {
-//   if (!response.ok) {
-//     if (response.status === 404) {
-//       throw new Error('No se encontro la data');
-//     } else if (response.status === 500) {
-//       throw new Error('Error de servidor');
-//     } else {
-//       throw new Error('No hubo una respuesta correcta');
-//     }
-//   }
-//   return response.json();
-// })
-// .then(data => {
-//   data.ganador.data.forEach(ele => {
-//     let piezas = ele["ENTIDAD_DISTRITO"].split("_");
-//     let cve = piezas[0].padStart(2,"0") + piezas[1].padStart(2,"0");
-//     ganadores.set(cve,{"color":ele["color"],"ganador":ele["img_partido"].replace(".png","")});
-//   })
-//   })
-// .catch(error => {
-//   console.error('Error: ', error);
-// });
-// let avanceDatos = fetch(avanceUrl).then(response => {
-//   if (!response.ok) {
-//     if (response.status === 404) {
-//       throw new Error('No se encontro la data de avance');
-//     } else if (response.status === 500) {
-//       throw new Error('Error de servidor de avance');
-//     } else {
-//       throw new Error('No hubo una respuesta correcta de avance');
-//     }
-//   }
-//   return response.json();
-// })
-// .then(data => {
-//   console.log("avance",data);
-//   avance.set("porcentaje",parseFloat(data.capturadas.porcentaje).toFixed(2));
-//   avance.set("corte",data.ultimoCorte.hora + " " + data.ultimoCorte.fecha);  
-//   d3.select("#corte").text(avance.get("corte"));
-//   d3.select("#porcentaje").text(avance.get("porcentaje"));
-//   })
-// .catch(error => {
-//   console.error('Error avance: ', error);
-// });
 
 const div = d3.select("#mapa");
 const width = parseInt(d3.select("#mapa").style("width"));
@@ -69,11 +16,15 @@ const datap = d3.json("./resources/datos/mexico300-2024.geojson");
 const datae = d3.json("./resources/datos/mexico300bordes-2024.geojson");
 const distdata = d3.csv("./resources/datos/distdata.csv");
 
+let listado = "";
+
 let pintar = Promise.all([datap,datae,distdata]).then(function(data) {
 
     let distritos = data[0];
     let estados = data[1];
     let datos = data[2];
+
+
 
     console.log(datos, distritos);
 
@@ -92,16 +43,13 @@ let pintar = Promise.all([datap,datae,distdata]).then(function(data) {
         .attr("class","distrito_group");
 
 
-        // console.log("ganadores", ganadores.get("0302"))
-
-    // console.log("disthex", disthex)    
     disthex
         .append("path")
             .attr("d",path)
             .attr("class","distrito")
             .attr("data-tippy-content",(dato)=>{ 
-              // console.log("tippy",dato.properties.distrito);
               var estaData = datos.filter(d=> d.CVEDIS == dato.properties.distrito )[0];
+              // console.log("tippy",dato.properties.distrito,estaData);
               var html = "";
               if(estaData) {
                 if (estaData.PHOTO_URL) {
@@ -146,6 +94,14 @@ let pintar = Promise.all([datap,datae,distdata]).then(function(data) {
           trigger: "click"
         });
 
+    for (let d in datos) {
+        if (datos[d].PHOTO_URL) {
+          listado+="<img width='200' src='photos/"+datos[d].PHOTO_URL+"'><div style='display: inline-block; background-color: "+datos[d].SKIN_TONE+"; width: 100px; height: 100px;'>perla</div><div style='display: inline-block; background-color: "+datos[d].DOMINANT+"; width: 100px; height: 100px;'>dominante</div></div>"
+        }
+      
+      
+    }        
+    
 
     return disthex;
 
@@ -159,16 +115,14 @@ let pintar = Promise.all([datap,datae,distdata]).then(function(data) {
     disthex.select("path").style("fill", d => {
       // console.log("d",d);
       let cve = d["properties"]["distrito"];
-      // let gandat = ganadores.get(cve);
-      // if (gandat) {
-      //   let color = gandat["color"];
-      //   return color
-      // }
+
       let dato = datamap.get(d["properties"]["distrito"].toString());
       // console.log(d["properties"]["distrito"],dato.SKIN_TONE)
       return dato ? dato["SKIN_TONE"] : "white" || "white";
   
     })
+
+    document.body.innerHTML+=listado
 
    //console.log("esto", disthex.select("path"))
 
