9d6b98070e035fb4831e943da1e69e47198c7221
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2024-08-29 00:51:14 -0600

    refactor: Update ColorCheckCard component to use avatar styling and improved card look

--- components/color-check-card/index.tsx
+++ components/color-check-card/index.tsx
@@ -9,17 +9,21 @@ interface ColorCheckCardProps {
 const ColorCheckCard = ({ districtData }: ColorCheckCardProps) => {
 
     return (
-        <div className="flex flex-col p-4 border border-base-200 shadow-sm">
+        <div className="flex flex-col items-center p-4 bg-base-200 border border-base-300 shadow">
             <div>
-                <Image
-                    src={`/pigmentocracia/photos/${districtData.PHOTO_URL}`}
-                    height={120}
-                    width={120}
-                    alt={districtData.NOMBRE_DIPUTADO_ELECTO_2024}
-                    unoptimized
-                />
+                <div className="avatar">
+                    <div className="w-32 rounded-full">
+                        <Image
+                            src={`/pigmentocracia/photos/${districtData.PHOTO_URL}`}
+                            height={128}
+                            width={128}
+                            alt={districtData.NOMBRE_DIPUTADO_ELECTO_2024}
+                            unoptimized
+                        />
+                    </div>
+                </div>
             </div>
-            <div className="grid grid-cols-3 text-xs">
+            <div className="grid grid-cols-3 gap-1 text-xs">
                 <div className="w-full h-20 p-2 border border-gray-900" style={{ backgroundColor: districtData.SKIN_TONE }}>
                     <div>
                         PERLA (STONE)
